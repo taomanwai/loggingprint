be4efdd9e973e784c2931de5cf41a2f778d8bd93
Tommy Tao <o_otaotao@hotmail.com>
2017-11-16 16:18:30 +0800

    The 1st commit

---
+++ loggingprint/loggingprint.py
@@ -0,0 +1,10 @@
+import logging
+import logging.handlers
+
+def loggingprint(*params):
+    text = ''
+    for param in params:
+        text += param + ' '
+    text = text.strip()
+
+    logging.info(text)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='loggingprint',
+    version='1.0.0',
+    packages=['loggingprint'],
+    url='https://github.com/taomanwai/loggingprint',
+    license='MIT',
+    author='Tommy Tao',
+    author_email='o_otaotao@hotmail.com',
+    description=''
+)
