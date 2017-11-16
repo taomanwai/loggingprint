6cf2979b289af8520c115c3c48093604d7aaefca
Tommy Tao <o_otaotao@hotmail.com>
2017-11-16 16:58:25 +0800

    added description to README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # loggingprint
+
+A simple utility to print log to file (not screen) based on logging
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='loggingprint',
-    version='1.0.0',
+    version='1.0.1',
     packages=['loggingprint'],
     url='https://github.com/taomanwai/loggingprint',
     license='MIT',
